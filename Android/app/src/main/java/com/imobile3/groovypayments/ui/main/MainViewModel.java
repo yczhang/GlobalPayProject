bf71e1c348f67b5a15a3e0bc6400e6b527e6d80b
Yichun Zhang <yichun.zhang@romtech.com>
2021-05-20 08:23:07 -0400

    move aync loading to mainview mode

--- Android/app/src/main/java/com/imobile3/groovypayments/ui/main/MainViewModel.java
+++ Android/app/src/main/java/com/imobile3/groovypayments/ui/main/MainViewModel.java
@@ -1,7 +1,54 @@
 package com.imobile3.groovypayments.ui.main;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.imobile3.groovypayments.MainApplication;
+import com.imobile3.groovypayments.concurrent.GroovyExecutors;
+import com.imobile3.groovypayments.data.model.Product;
+import com.imobile3.groovypayments.network.WebServiceManager;
+import com.stripe.exception.StripeException;
+import com.stripe.model.ProductCollection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    public void getProducts() {
+        GroovyExecutors.getInstance().getNetwork().execute(() -> {
+            try {
+                WebServiceManager.getInstance().init(MainApplication.getInstance().getWebServiceConfig());
+
+                Map<String, Object> params = new HashMap<>();
+
+                params.put("limit", 20);
+
+                ProductCollection products = com.stripe.model.Product.list(params);
+
+                List<Product> resultSet = new ArrayList<Product>();
+
+                for ( com.stripe.model.Product product : products.getData()) {
+
+                    Product newProduct = new Product();
+
+                    newProduct.setName(product.getName());
+                    newProduct.setDesc(product.getDescription());
+
+                    if(product.getImages() != null && product.getImages().size() > 0) {
+                        newProduct.setImageName(product.getImages().get(0));
+                    }
+                    resultSet.add(newProduct);
+                }
+
+
+
+            } catch (StripeException e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
